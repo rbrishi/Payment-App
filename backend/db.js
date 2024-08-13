90b157f40f32dfaddf6eb2ec8dd7de5b3a633afa
rbrishi <brishabh100@gmail.com>
2024-08-13 19:29:39 +0530

    some changes

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+// Create a Schema for Users
 const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  firstName: String,
-  lastName: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
 });
 
-const User = mongoose.model('User', userSchema);
+// Create a model from the schema
+const User = mongoose.model("User", userSchema);
 
 module.exports = {
-    User
+  User,
 };
