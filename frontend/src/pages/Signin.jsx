e9244381bb8701e3a7f786fd8381c11ac24ec5f6
Rishabh Bajpai <98521911+rbrishi@users.noreply.github.com>
2024-11-10 22:56:53 +0530

    Update Signin.jsx

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -10,7 +10,7 @@ export const Signin = () => {
       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
         <Heading label={"Sign in"} />
         <SubHeading label={"Enter your credentials to access your account"} />
-        <InputBox placeholder="harkirat@gmail.com" label={"Email"} />
+        <InputBox placeholder="rishabh@gmail.com" label={"Email"} />
         <InputBox placeholder="123456" label={"Password"} />
         <div className="pt-4">
           <Button label={"Sign in"} />
