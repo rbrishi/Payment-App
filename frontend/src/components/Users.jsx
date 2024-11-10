79338bcc68111d6bdc88d9eccbceb2c3e1c818ac
Rishabh Bajpai <98521911+rbrishi@users.noreply.github.com>
2024-11-10 22:58:37 +0530

    Update Users.jsx

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -10,7 +10,7 @@ export const Users = () => {
     const [filter, setFilter] = useState("");
 
     useEffect(() => {
-        axios.get("http://localhost:3000/api/v1/user/bulk?filter=" + filter)
+        axios.get("https://payment-app-backend-nchy.onrender.com/api/v1/user/bulk?filter=" + filter)
             .then(response => {
                 setUsers(response.data.user)
             })
